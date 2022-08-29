b55b847483f115c37e26f8f604975c6757c6df4f
basmah saad <basmahkw7@icloud.com>
2022-08-30 00:11:49 +0300

    go

---
+++ JOYS/JOYS/JOYSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JOYSApp.swift
+//  JOYS
+//
+//  Created by basmah saad on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct JOYSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
